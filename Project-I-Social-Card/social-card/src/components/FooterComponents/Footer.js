82dad326d164f2192d1eb68dfe27a92f64be6366
Karim B <mattiakarim@live.it>
2019-05-27 17:27:44 +0200

    Fix color anchor tag

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,8 +5,11 @@ import FooterIcons from './FooterContent.jsx';
 const Footer = () => {
     return(
         <div> 
-           <FooterIcons class="first-icon" type="mode_comment"/>
-           <FooterIcons type="sync" num="4"/>
+           <FooterIcons onClick={event => {
+        event.preventDefault();
+        console.log('I was clicked');
+    }} class="first-icon" type="mode_comment"/>
+           <FooterIcons type="sync" num="4"></FooterIcons>
            <FooterIcons type="favorite_border" num="6"/>    
            <FooterIcons type="mail_outline"/>
         </div>

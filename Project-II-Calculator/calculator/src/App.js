0c066b80f5422a6ed22889bcbc572f4581fd3f43
Karim B <mattiakarim@live.it>
2019-05-28 19:27:02 +0200

    Calculator is now working

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -5,21 +5,43 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
-  const [total, setTotal] = useState(0);
+  const [total, updatedTotal] = useState(0);
+
+  const clearReset = () => {
+    updatedTotal(0); 
+  }
+
+  const calculate = (calc) => {
+    return new Function(`return ${calc}`)();
+  }
 
   const displayedValue = (event) => {
-    if(event.target.textContent === 'clear'){
-      setTotal(0);
-    } else {
-      setTotal(`${event.target.textContent}`)
+    let inputValue = event.target.textContent;
+
+    if(total === 0) {
+      return updatedTotal(inputValue);
     }
+
+    if(inputValue === '÷') {
+      return updatedTotal(total + '/');
+    } 
+
+    if(inputValue === 'x') {
+      return updatedTotal(total + '*')
+    }
+
+    if(inputValue === '=') {
+      return updatedTotal(calculate(total));
+    }
+
+    return updatedTotal(total + inputValue);
   }
 
   return (
     <div className="calculator">
       <CalculatorDisplay tot={total}/>
       <div className="btn-wrapper">
-        <ActionButton clicked={displayedValue} content="clear"/>
+        <ActionButton clicked={clearReset} content="clear"/>
         <NumberButton clicked={displayedValue} btnStyle="calc-btn" bgColor="#A0001E" color="#fff" content="÷"/>
         {[9, 8, 7].map(num => <NumberButton key={num} clicked={displayedValue} btnStyle="num-btn" content={num}/>)}
         <NumberButton clicked={displayedValue} btnStyle="calc-btn" bgColor="#A0001E" color="#fff" content="x"/>
